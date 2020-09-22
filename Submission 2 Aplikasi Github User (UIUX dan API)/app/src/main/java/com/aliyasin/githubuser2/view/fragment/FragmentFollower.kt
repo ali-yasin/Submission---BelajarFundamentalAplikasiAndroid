9fd146b98ec17bb2f5ed2afdc43cf28561442e1f
Aliydv <aliyasin.dev@gmail.com>
2020-09-23 04:24:32 +0700

    add file

---
+++ Submission 2 Aplikasi Github User (UIUX dan API)/app/src/main/java/com/aliyasin/githubuser2/view/fragment/FragmentFollower.kt
@@ -0,0 +1,72 @@
+package com.aliyasin.githubuser2.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aliyasin.githubuser2.R
+import com.aliyasin.githubuser2.model.DataFollowers
+import com.aliyasin.githubuser2.model.DataUsers
+import com.aliyasin.githubuser2.viewModel.FollowerViewModel
+import com.aliyasin.githubuser2.viewModel.ListDataFollowerAdapter
+import kotlinx.android.synthetic.main.fragment_follower.*
+
+class FragmentFollower : Fragment() {
+
+    companion object {
+        val TAG = FragmentFollower::class.java.simpleName
+        const val EXTRA_DETAIL = "extra_detail"
+    }
+
+    private val listData: ArrayList<DataFollowers> = ArrayList()
+    private lateinit var adapter: ListDataFollowerAdapter
+    private lateinit var followerViewModel: FollowerViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_follower, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = ListDataFollowerAdapter(listData)
+        followerViewModel = ViewModelProvider(
+            this, ViewModelProvider.NewInstanceFactory()
+        ).get(FollowerViewModel::class.java)
+
+        val dataUser = activity!!.intent.getParcelableExtra(EXTRA_DETAIL) as DataUsers
+        config()
+
+        followerViewModel.getDataGit(activity!!.applicationContext, dataUser.username.toString())
+        showLoading(true)
+
+        followerViewModel.getListFollower().observe(activity!!, Observer { listFollower ->
+            if (listFollower != null) {
+                adapter.setData(listFollower)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun config() {
+        recyclerViewFollowers.layoutManager = LinearLayoutManager(activity)
+        recyclerViewFollowers.setHasFixedSize(true)
+        recyclerViewFollowers.adapter = adapter
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            progressbarFollowers.visibility = View.VISIBLE
+        } else {
+            progressbarFollowers.visibility = View.INVISIBLE
+        }
+    }
+}
---
+++ Submission 2 Aplikasi Github User (UIUX dan API)/app/src/main/java/com/aliyasin/githubuser2/view/fragment/FragmentFollowing.kt
@@ -0,0 +1,76 @@
+package com.aliyasin.githubuser2.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aliyasin.githubuser2.R
+import com.aliyasin.githubuser2.model.DataFollowing
+import com.aliyasin.githubuser2.model.DataUsers
+import com.aliyasin.githubuser2.viewModel.FollowingViewModel
+import com.aliyasin.githubuser2.viewModel.ListDataFollowingAdapter
+import kotlinx.android.synthetic.main.fragment_following.*
+
+class FragmentFollowing : Fragment() {
+
+    companion object {
+        val TAG = FragmentFollowing::class.java.simpleName
+        const val EXTRA_DETAIL = "extra_detail"
+    }
+
+    private var bool: Boolean = false
+    private var listData: ArrayList<DataFollowing> = ArrayList()
+    private lateinit var adapter: ListDataFollowingAdapter
+    private lateinit var followingViewModel: FollowingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_following, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = ListDataFollowingAdapter(listData)
+        followingViewModel = ViewModelProvider(
+            this, ViewModelProvider.NewInstanceFactory()
+        ).get(FollowingViewModel::class.java)
+
+        val dataUser = activity!!.intent.getParcelableExtra(EXTRA_DETAIL) as DataUsers
+        config()
+
+        followingViewModel.getDataGit(
+            activity!!.applicationContext,
+            dataUser.username.toString()
+        )
+        showLoading(true)
+
+        followingViewModel.getListFollowing().observe(activity!!, Observer { listFollowing ->
+            if (listFollowing != null) {
+                adapter.setData(listFollowing)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun config() {
+        recyclerViewFollowing.layoutManager = LinearLayoutManager(activity)
+        recyclerViewFollowing.setHasFixedSize(true)
+        recyclerViewFollowing.adapter = adapter
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            progressbarFollowing.visibility = View.VISIBLE
+        } else {
+            progressbarFollowing.visibility = View.INVISIBLE
+        }
+    }
+}
